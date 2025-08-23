db7e6799e96eee65274ca384c210d46ba7a1397b
Barry Brook <barry.brook@utas.edu.au>
2025-08-23 21:17:29 +1000

    targets: source R/ for dev runs; remove package self-load; ignore _targets dir; Dryad fetch via file_stream id; gz-or-zip ingest

--- _targets.R
+++ _targets.R
@@ -1,9 +1,11 @@
 library(targets)
-tar_option_set(packages = c("SpeciesRichness","data.table","cli","glue"))
+# Source package R/ functions for dev workflow without requiring installation
+tarchetypes::tar_source("R")
+tar_option_set(packages = c("data.table","cli","glue"))
 
 list(
-  tar_target(dryad_zip, SpeciesRichness::fetch_ecoregister_zip(version = "v20250703"), format = "file"),
-  tar_target(counts_dt, SpeciesRichness::read_ecoregister_counts(dryad_zip)),
-  tar_target(per_inv_sn, SpeciesRichness::summarise_per_inventory(counts_dt)),
-  tar_target(co_sample_flat, SpeciesRichness::build_co_sample_flat(counts_dt), format = "file")
+  tar_target(dryad_zip, fetch_ecoregister_zip(version = "v20250703"), format = "file"),
+  tar_target(counts_dt, read_ecoregister_counts(dryad_zip)),
+  tar_target(per_inv_sn, summarise_per_inventory(counts_dt)),
+  tar_target(co_sample_flat, build_co_sample_flat(counts_dt), format = "file")
 )
